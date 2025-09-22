cbc4553aec5d8692b086061f5b73ada9587baf2f
Fatih Simsek <fatih.simsek5209@gmail.com>
2025-09-22 18:38:53 +0200

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,165 @@
+// @ts-check
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'How to?',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  future: {
+    v4: true,
+  },
+
+  url: 'https://your-docusaurus-site.example.com',
+
+  baseUrl: '/',
+
+  organizationName: 'fatih5252',
+  projectName: 'how-to',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: 'docs',
+          routeBasePath: 'docs',
+          editUrl: ({ locale, docPath }) => {
+            if (locale === 'de') {
+              return `https://github.com/fatih5252/how-to/tree/main/i18n/de/docusaurus-plugin-content-docs/current/${docPath}`;
+            }
+            return `https://github.com/fatih5252/how-to/tree/main/docs/${docPath}`;
+          },
+          sidebarPath: './sidebars.js',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          editUrl: ({ blogDirPath, blogPath, permalink }) => {
+            return `https://github.com/fatih5252/how-to/tree/main/${blogDirPath}/${blogPath}`;
+          },
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'How to?',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/favicon.ico',
+        },
+        items: [
+          {
+            label: 'Intro',
+            to: '/docs/Intro',
+            position: 'left',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/fatih5252/how-to',
+            label: 'GitHub',
+            className: 'header-github-link',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'How to?',
+            items: [
+              {
+                label: 'Privacy',
+                to: '/privacy',
+              },
+              {
+                label: 'Disclaimer',
+                to: '/disclaimer',
+              },
+              {
+                label: 'Old Project',
+                href: 'https://old.scnx-changelog.de'
+              }
+            ],
+          },
+          {
+            title: 'Useful Links',
+            items: [
+              {
+                label: 'Disocrd (scootkit german)',
+                href: 'https://discord.gg/jzPpfjUAct',
+              },
+              {
+                label: 'Discord (scootkit english)',
+                href: 'https://discord.gg/RV4xSSdqT6'
+              },
+              {
+                label: 'Discord (changelog)',
+                href: 'https://discord.gg/XZGGb27zdE',
+              },
+              {
+                label: 'SCNX (referral link)',
+                href: 'https://scnx.app/referral?code=MHtz3IGIns4a8ntj0Dc7sk8kwnqWIP2D2SIC',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'SCNX Docs',
+                href: 'https://docs.scnx.xyz',
+              },
+              {
+                label: 'SCNX tips',
+                href: 'https://scnx.tips',
+              },
+              {
+                label: 'SCNX Faq',
+                href: 'https://faq.scnx.app',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/pages/index.js
@@ -0,0 +1,62 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import '../css/custom.css';
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+import Translate from '@docusaurus/Translate';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">
+          <Translate
+            id="homepage.taglineText"
+            description="The tagline text displayed on the homepage">
+            In this project, we will explain how to setup a module properly in SCNX.
+          </Translate>
+        </p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/Intro">
+            <Translate
+              id="homepage.goToChangelogButton"
+              description="Text for the 'Go to Changelog' button">
+              Go to How to!
+            </Translate>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Changelog - ${siteConfig.title}`}
+      description={
+        <Translate
+          id="layout.homepageDescription"
+          description="How to?">
+          How to?
+        </Translate>
+      }>
+      <HomepageHeader />
+      <main />
+      <SpeedInsights />
+      <Analytics />
+    </Layout>
+  );
+}
