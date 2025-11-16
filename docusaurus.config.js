c193aa3ea40702e3afce1caf436b5d2849c79eec
Fatih Simsek <fatih.simsek5209@gmail.com>
2025-11-16 15:09:44 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
           },
           {
             type: 'dropdown',
-            label: 'Howto projects',
+            label: 'HowTo projects',
             position: 'right',
             items: [
               {
